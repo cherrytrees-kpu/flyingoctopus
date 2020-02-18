670dc91de92f0c4e770f7cc8725c7b628db06942
cherrytrees-kpu <54606285+cherrytrees-kpu@users.noreply.github.com>
2020-02-18 15:41:13 -0800

    Ensembl sequences
    Program is now capable of getting all of the affected sequences. Program places these sequences in a folder named by the variant.

--- va.py
+++ va.py
@@ -35,6 +35,8 @@ def importHGVS(filename):
     for line in inputfile:
         listHGVS.append(line.strip('\n'))
 
+    inputfile.close()
+
     return listHGVS
 
 def importanno(filename):
@@ -125,7 +127,7 @@ def outputHGVS(listHGVS, name = ""):
             outputfile.write('\n')
         i = i + 1
 
-    outputFile.close()
+    outputfile.close()
 
 def exportanno(listanno, filename):
     """
@@ -257,7 +259,14 @@ def annotmvi(listHGVS):
                                             'dbsnp.vartype',
                                             'dbsnp.gene',
                                             'clinvar',
-                                            'gnomad_genome.af', 'gnomad_exome.af']))
+                                            'gnomad_genome.af',
+                                            'gnomad_exome.af',
+                                            'dbnsfp.ensembl',
+                                            'dbnsfp.uniprot',
+                                            'dbnsfp.polyphen2',
+                                            'dbnsfp.sift',
+                                            'dbnsfp.provean',
+                                            ]))
 
         #Progress tracker
         if listHGVS.index(idHGVS)%100 == 0:
@@ -377,6 +386,20 @@ def annothpa(data):
         i = i + 1
 
     return expression
+
+def ensemblsequence(transcriptid, seq):
+    server = 'https://rest.ensembl.org'
+    ext = '/sequence/id/' + transcriptid + '?'
+
+    xheaders={'Content-Type':'application/json'}
+    xparams = {'type':seq,'object_type':'transcript'}
+
+    r = requests.get(server+ext, headers=xheaders, params=xparams)
+
+    if r.status_code == 200:
+        return r.json()['seq']
+    else:
+        return None
 
 ##### Parsing Functions ########################################################
 def dumpCV(anno_mvi):
@@ -606,6 +629,78 @@ def combineanno(listmvi, listvep, listHGVS):
         i = i + 1
 
     return listanno
+
+def converthgvs(idHGVS):
+    """
+    converthgvs - convert a HGVS ID into a format that can be used in a filename
+    or directory
+    Parameters: idHGVS - the HGVS ID as a string
+    Return: a string with the converted HGVS ID
+    """
+    def findchr(idHGVS):
+        if idHGVS[3:5].isnumeric() is True:
+            return idHGVS[3:5]
+        else:
+            return idHGVS[3]
+    def findpos(idHGVS, marker):
+        pos_start = idHGVS.index(marker) + 1
+        pos_end = pos_start + 1
+        pos_complete = False
+
+        while pos_complete is False:
+            if idHGVS[pos_start:pos_end].isnumeric() is False:
+                pos_chr = idHGVS[pos_start:(pos_end-1)]
+                pos_complete = True
+            else:
+                pos_end = pos_end + 1
+        return pos_chr
+    #For substitution
+    if '>' in idHGVS:
+        #Identify chromosome number
+        num_chr = findchr(idHGVS)
+        #Identify position
+        pos_chr = findpos(idHGVS, '.')
+        #Identify allele 1 and 2
+        allele1 = idHGVS[idHGVS.index('>') - 1]
+        allele2 = idHGVS[idHGVS.index('>') + 1]
+        return (num_chr + '-' + pos_chr + '-' + allele1 + '-' + allele2)
+
+    #For deletion and insertion
+    elif 'delins' in idHGVS:
+        #Identify chromosome numbers
+        num_chr = findchr(idHGVS)
+        #Identify position
+        pos_chr = findpos(idHGVS, '.')
+        #Identify inserted sequence
+        seq_delins = idHGVS[idHGVS.index('s') + 1:]
+        #Debug
+        return(num_chr + '-' + pos_chr + '-delins-' + seq_delins)
+
+    #For deletion
+    elif 'del' in idHGVS:
+        #Identify chromosome number
+        num_chr = findchr(idHGVS)
+        #Identify position 1
+        if '_' in idHGVS:
+            pos1_chr = idHGVS[(idHGVS.index('.') + 1):idHGVS.index('_')]
+            #Identify position 2
+            pos2_chr = findpos(idHGVS, '_')
+            return (num_chr + '-' + str(pos1_chr) + '_' + str(pos2_chr) + '-del')
+        else:
+            pos_chr = findpos(idHGVS, '.')
+            return (num_chr + '-' + str(pos_chr) + '-del')
+
+    #For insertion
+    elif 'ins' in idHGVS:
+        #Identify chromosome number
+        num_chr = findchr(idHGVS)
+        #Identify positions
+        pos1_chr = idHGVS[(idHGVS.index('.') + 1):idHGVS.index('_')]
+        pos2_chr = findpos(idHGVS, '_')
+        #Identify inserted sequences
+        seq_ins = idHGVS[idHGVS.index('s') + 1:]
+        return (num_chr + '-' + str(pos1_chr) + '_' + str(pos2_chr) + '-ins' + seq_ins)
+
 ##### Processing Functions #####################################################
 def filteraffected(listaffected, listcontrol):
     """
